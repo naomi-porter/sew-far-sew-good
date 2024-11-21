9c119881923e6210807c2a14cb90d2e6b2994621
naomi-porter <naomiporter.dev@gmail.com>
2024-11-21 18:28:10 +1300

    downgrade Chakra UI fixes typescript compatability issue

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react';
+
 function App() {
   return (
     <>
+    <ChakraProvider>
       <header className="header">
-        <h1>My Collection</h1>
+        <h1>My Collection ho hay</h1>
       </header>
       <section className="main">{/* add your code here */}</section>
+      </ChakraProvider>
     </>
   )
 }
